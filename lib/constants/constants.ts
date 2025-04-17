3a471a0458af7941674fed8b45430a210b65d11e
Abhilash <abhilashreddy.chitiki@sithafal.com>
2025-04-17 17:46:54 -0500

    Add sign up functionality and user authentication components

--- lib/constants/constants.ts
+++ lib/constants/constants.ts
@@ -2,3 +2,9 @@ export const signInDefaultValues = {
   email: "admin@prostore.com",
   password: "admin123",
 };
+export const signUpDefaultValues = {
+  name: "John abhram",
+  email: "John@example.com",
+  password: "password",
+  confirmPassword: "password",
+};
