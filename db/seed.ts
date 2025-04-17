f22eb948d63f719a50f486a173adc78fb838984e
Abhilash <abhilashreddy.chitiki@sithafal.com>
2025-04-17 10:12:54 -0500

    added user data and next-auth-setup

--- db/seed.ts
+++ db/seed.ts
@@ -3,28 +3,16 @@ import sampleData from "./sample-data";
 
 async function main() {
   const prisma = new PrismaClient();
-  try {
-    console.log("Deleting existing products...");
-    await prisma.product.deleteMany();
+  await prisma.product.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
 
-    console.log("Seeding products...");
-    // Prisma will handle the conversion to Decimal
-    await prisma.product.createMany({
-      data: sampleData.products,
-    });
+  await prisma.user.createMany({ data: sampleData.users });
+  await prisma.product.createMany({ data: sampleData.products });
 
-    console.log("Database seeded successfully");
-  } catch (error) {
-    console.error("Error seeding database:", error);
-    process.exit(1);
-  } finally {
-    await prisma.$disconnect();
-  }
+  console.log("Database seeded successfully");
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error("Error in seed script:", error);
-    process.exit(1);
-  });
+main();
