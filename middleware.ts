faaeb64d48e4806df40da85a12c5873bfd8195c2
Abhilash <abhilashreddy.chitiki@sithafal.com>
2025-04-17 22:31:34 -0500

    Fix middleware to avoid using PrismaClient in Edge Functions

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,36 @@
-export { auth as middleware } from '@/auth';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  // Check for cart cookie
+  if (!request.cookies.get("sessionCartId")) {
+    // Generate cart cookie
+    const sessionCartId = crypto.randomUUID();
+
+    // Get the response
+    const response = NextResponse.next();
+
+    // Set the newly generated sessionCartId in the response cookies
+    response.cookies.set("sessionCartId", sessionCartId);
+
+    // Return the response with the sessionCartId set
+    return response;
+  }
+
+  // If the cookie exists, just continue
+  return NextResponse.next();
+}
+
+// Configure which paths the middleware runs on
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
